c8a62708ed67f6008105365123427bdcd96aedb4
Ondřej Štauda <staudond@fit.cvut.cz>
2020-03-09 13:41:04 +0100

    Added properties to list stats of enemies and units, started to work on custom tiles for Enemies and units

---
+++ Assets/Properties/EnemyProperties.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProperties
+{
+    //Goblin properties
+    public const int GoblinDmg = 1;
+    public const int GoblinHp = 20;
+    public const int GoblinSpeed = 4;
+    
+    //Orc properties
+    public const int OrcDmg = 5;
+    public const int OrcHp = 60;
+    public const int OrcSpeed = 1;
+}
---
+++ Assets/Properties/UnitProperties.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitProperties
+{
+    //Swordman properties
+    public const int SwordmanDmg = 1;
+    public const int SwordmanHp = 100;
+    public const int SwordmanSpeed = 2;
+}
