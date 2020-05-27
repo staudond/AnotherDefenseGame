891fb18e35ae3c016ba521191dcbd103d11858cd
Ondřej Štauda <staudond@fit.cvut.cz>
2020-05-28 00:37:56 +0200

    added new properties

--- Assets/Properties/EnemyProperties.cs
+++ Assets/Properties/EnemyProperties.cs
@@ -7,10 +7,15 @@ public class EnemyProperties
     //Goblin properties
     public const int GoblinDmg = 1;
     public const int GoblinHp = 20;
-    public const int GoblinSpeed = 4;
+    public const int GoblinAttackSpeed = 4;
+    public const int GoblinVitality = 30;
+    public const int GoblinGoldValue = 25;
+    
     
     //Orc properties
     public const int OrcDmg = 5;
     public const int OrcHp = 60;
-    public const int OrcSpeed = 1;
+    public const int OrcAttackSpeed = 1;
+    public const int OrcVitality = 15;
+    public const int OrcGoldValue = 60;
 }
--- Assets/Properties/UnitProperties.cs
+++ Assets/Properties/UnitProperties.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitProperties {
+    
+    //attack range 1: 1 up, 1 down, 1 left, 1 right
+    //attack range 2: 2 up, 2 down, 2 left, 2 right, 1 diagonally
+    //the range is rhombus shaped
+    
+    //Attack speed - number of attack creature can do at once when attacks
+    //Attack cooldown - number of turns before creature can attack
+    
     public const int UnitMovementRange = 1;
     //Swordman properties
-    public const int SwordmanDmg = 1;
+    public const int SwordmanDmg = 5;
     public const int SwordmanHp = 100;
     public const int SwordmanAttackSpeed = 2;
+    public const int SwordmanAttackRange = 1; 
+    public const int SwordmanAttackCooldown = 1;
 }
