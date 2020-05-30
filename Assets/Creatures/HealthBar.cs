d354e7deb4727809438bdb93afe073ea56fcd065
Ondřej Štauda <staudond@fit.cvut.cz>
2020-05-30 12:49:59 +0200

    added HEALTHBAR

---
+++ Assets/Creatures/HealthBar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+    private int maxHealth;
+    private BasicCreature creature;
+    private Transform bar;
+    private Color normalColor;
+    private Color injuredColor;
+    
+
+    void Start()
+    {
+        creature = transform.parent.gameObject.GetComponentInChildren<BasicCreature>();
+        bar = transform.Find("Bar");
+        maxHealth = creature.MaxHealth;
+        creature.OnHealthChanged += CreatureOnOnHealthChanged;
+        normalColor = new Color(0f,1f,0f,0.6f);
+        injuredColor = new Color(1f,0f,0f,0.6f);
+        
+    }
+
+    private void CreatureOnOnHealthChanged(object sender, EventArgs e) {
+        float percentage = creature.GetHealthPercent();
+        if (percentage < 0.5f) {
+            Debug.Log('s');
+            bar.Find("BarSprite").GetComponent<SpriteRenderer>().color = injuredColor;
+        }
+        
+        bar.localScale= new Vector3(percentage, 1);
+    }
+
+
+ 
+    void Update() {
+       
+    }
+}
